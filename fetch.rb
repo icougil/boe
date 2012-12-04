c95bdb4d4a310b3c0fe531f23e7efdf2eeb6b071
David Cabo <david.cabo@gmail.com>
2012-12-05 00:24:53 +0100

    Improve script to detect latest available announcement automatically

--- fetch.rb
+++ fetch.rb
@@ -1,21 +1,48 @@
+# NOTE: Currently we are only fetching the BOE-A series
+
 require 'rubygems'
 require 'mechanize'
 require 'fastercsv'
 require 'nokogiri'
 require 'open-uri'
 
-agent = Mechanize.new
+class BOESpider
+  def initialize
+    @agent = Mechanize.new
+  end
 
-# TODO: Find out maximum item number dynamically
-# TODO: Section B
-# TODO: Other years
-year = 2012
-1.upto(14783) do |i|
-  filename = "data/BOE-A-#{year}-#{i}.html"
-  unless File.exists?(filename)
-    puts "Downloading BOE item BOE-A-#{year}-#{i}..."
-    boe_page = agent.get("http://www.boe.es/buscar/doc.php?id=BOE-A-#{year}-#{i}")
-    File.open(filename, 'w') {|f| f.write( boe_page.content ) }
-    sleep(2)
+  def get_latest_announcement_number(year)
+    # Get a link to the latest available BOE of the year
+    puts "Fetching publication calendar for year #{year}..."
+    calendar = @agent.get("http://www.boe.es/diario_boe/calendarios.php?a=#{year}")
+    latest_boe_link = calendar.links_with(:href => /dias/).last
+
+    # The link is just to a section of the BOE, not the full summary, so add a parameter
+    puts "Fetching latest BOE for year #{year}..."
+    latest_boe_link.uri.query='s=c'
+    latest_boe = @agent.get(latest_boe_link.uri)
+    latest_announcement = latest_boe.links_with(:href => /BOE-A/).last
+
+    # We got it, the link to the last announcement. Extract the announcement number.
+    latest_announcement.href =~ /-(\d+)$/
+    $1.to_i
+  end
+
+  def fetch_year(year)
+    latest_announcement = get_latest_announcement_number(year)
+    puts "Got latest announcement id for year #{year}: #{latest_announcement}..."
+
+    1.upto(latest_announcement) do |i|
+      filename = "data/BOE-A-#{year}-#{i}.html"
+      unless File.exists?(filename)
+        puts "Downloading BOE item BOE-A-#{year}-#{i}..."
+        boe_page = @agent.get("http://www.boe.es/buscar/doc.php?id=BOE-A-#{year}-#{i}")
+        File.open(filename, 'w') {|f| f.write( boe_page.content ) }
+        sleep(2)
+      end
+    end
   end
 end
+
+spider = BOESpider.new()
+spider.fetch_year(2012)
