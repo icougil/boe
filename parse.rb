8075e092b40e29fedad54ad34c9604c422f80738
David Cabo <david.cabo@gmail.com>
2012-12-05 20:19:51 +0100

    Extract some more pardon info, and add basic tests

--- parse.rb
+++ parse.rb
@@ -8,21 +8,46 @@ require 'csv'
 # So I can tail the output file in real time when redirecting
 STDOUT.sync = true
 
-def get_BOE_id(filename)
-  filename =~ /(BOE-.-\d{4}-\d+)/
-  $1
-end
+module PardonParser
+  def self.get_BOE_id(filename)
+    filename =~ /(BOE-.-\d{4}-\d+)/
+    $1
+  end
 
-def parse_file(filename)
-  doc = Nokogiri::HTML(open(filename))
-  title = doc.search(".documento-tit").text
+  def self.get_military_trial_details(p)
+  end
 
-  # TODO: Pardon text doc.search("p.parrafo"). Signature: doc.search("p.parrafo_2")
+  def self.get_trial_details(p)
+    if p =~ /deserción/
+      return get_military_trial_details(p)
+    end
 
-  if title =~ /se indulta a (do.*)\./ or title =~ /se concede el indulto .*? (do(n|ña) .*)\./
-    puts "#{get_BOE_id(filename)},#{$1}"
+    p =~ /en sentencia de (\d+ de [a-z]+ de.\d{4}), (.*)$/
+    trial_date = $1
+    left_over = $2
+
+    return trial_date
+  end
+
+  def self.parse_file(filename)
+    doc = Nokogiri::HTML(open(filename))
+    title = doc.search(".documento-tit").text
+    # TODO: Should probably not get the person name from the title: be more relaxed 
+    # in regex to avoid missing odd ones
+    if title =~ /se indulta a (do.*)\./ or title =~ /se concede el indulto .*? (do(n|ña) .*)\./
+      name = $1 # Matches the name in the title
+
+      # Get other relevant details from document
+      department = doc.search('.valDoc')[2].text
+      first_paragraph, second_paragraph = doc.search("p.parrafo")
+      get_trial_details(first_paragraph.text)
+
+      # We get the BOE date from the PDF url, easier than parsing the expanded human readable date
+      pdf = doc.search('.puntoPDF a').first.attributes['href'].value
+      pdf =~ /dias\/(\d{4})\/(\d{2})\/(\d{2})/
+      year, month, day = $1, $2, $3
+
+      # puts CSV::generate_line([get_BOE_id(filename), "#{day}-#{month}-#{year}", department, name])
+    end
   end
 end
-
-puts 'File,Title'
-Dir['data/*.html'].each {|filename| parse_file(filename)}
