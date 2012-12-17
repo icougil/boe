86f82675736cf89b49fa2b8927ff0664ebf4e587
Juan Elosua <juan.elosua@gmail.com>
2012-12-17 18:27:55 +0100

    Generate pardon statements output and debug file

--- parse_pardons.rb
+++ parse_pardons.rb
@@ -1,16 +1,38 @@
+#!/usr/bin/env ruby
 # encoding: UTF-8
 
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'fileutils'
 require 'csv'
 
 require './lib/PardonParser.rb'
 
-# So I can tail the output file in real time when redirecting
-STDOUT.sync = true
+HEADER = 'BOE,Fecha,Departamento,Nombre,Fecha condena,Papel,Crímen,Sentencia,Año crimen,Tipo indulto,Reducción/Nueva condena,Condición'
+LOG_SUBDIR = 'logs'
+OUTPUT_FILES_SUBDIR = 'output'
+YEAR = ARGV[0]
+FileUtils.makedirs(LOG_SUBDIR)
+FileUtils.makedirs(OUTPUT_FILES_SUBDIR)
 
-puts 'BOE,Fecha,Departamento,Nombre,Tribunal,Fecha condena,Papel,Crímen,Categoría crimen,Sentencia,Año crimen,Tipo indulto,Reducción/Nueva condena,Condición'
-Dir['data/*.html'].each do |filename| 
-  PardonParser.parse_file Nokogiri::HTML(open(filename))
+$log_file = File.open("#{LOG_SUBDIR}/parse_pardons#{YEAR}.log", 'w')
+#Detected pardon statements file
+$output_file = File.open("#{OUTPUT_FILES_SUBDIR}/Indultos#{YEAR}.csv", 'w')
+$output_file.puts HEADER
+#Detected pardon statements with unhandled fields file for manual review
+$output_debug_file = File.open("#{OUTPUT_FILES_SUBDIR}/IndultosDebug#{YEAR}.csv", 'w')
+$output_debug_file.puts HEADER
+count = 0
+pardons = 0
+Dir["../#{YEAR}/*.html"].each do |filename| 
+  puts "parsed #{count} BOE documents" if (count != 0 and count % 1000 == 0)
+  result = PardonParser.parse_file Nokogiri::HTML(open(filename))
+  pardons += 1 if result
+  count += 1
 end
+puts "#{pardons} pardon statements correctly detected."
+$log_file.close
+$output_file.close
+$output_debug_file.close
+
