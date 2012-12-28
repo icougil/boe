7dba1c20eb1557fb6fadeca335f13652d1cc2ea6
Juan Elosua <juan.elosua@gmail.com>
2012-12-28 20:29:25 +0100

    Added court and category to the header

--- parse_pardons.rb
+++ parse_pardons.rb
@@ -9,7 +9,7 @@ require 'csv'
 
 require './lib/PardonParser.rb'
 
-HEADER = 'BOE,Fecha,Departamento,Nombre,Fecha condena,Papel,Crímen,Sentencia,Año crimen,Tipo indulto,Reducción/Nueva condena,Condición'
+HEADER = 'BOE,Fecha,Departamento,Nombre,Tribunal,Fecha condena,Papel,Crimen,Categoría crimen,Sentencia,Año crimen,Tipo indulto,Reducción/Nueva condena,Condición'
 LOG_SUBDIR = 'logs'
 OUTPUT_FILES_SUBDIR = 'output'
 YEAR = ARGV[0]
